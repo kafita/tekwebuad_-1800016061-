ca4eac0ad33f251c00a4a0a1aca6f1cf7e5e7551
kafita <kafita1800016061@webmail.uad.ac.id>
2020-04-01 20:45:49 +0700

    update baru

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './halaman-utama.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'tekwebb1800016061';
+}
